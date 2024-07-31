20ff855cd4afa41f80633bc2fc2d640f5e5fc3b8
watchiswatch <chchih99@gmail.com>
2024-07-31 19:21:18 +0900

    Wrapper

--- src/lib/Marquee.tsx
+++ src/lib/Marquee.tsx
@@ -3,12 +3,23 @@ import "./styles/index.css";
 
 interface Props extends React.HTMLProps<HTMLDivElement> {
   children: React.ReactNode;
+  cb: () => void;
 }
 
-function Marquee({ children, ...rest }: Props) {
+function Marquee({ children, cb, ...rest }: Props) {
   return (
-    <div className="marquee-left" {...rest}>
-      {children}
+    <div {...rest}>
+      <div
+        className="marquee-left"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+        onAnimationEnd={cb}
+      >
+        {children}
+      </div>
     </div>
   );
 }
