2c94e893543aa3cee6d8c5fd5bf25bb6521b5d5a
watchiswatch <chchih99@gmail.com>
2024-07-31 19:08:30 +0900

    animation

--- src/lib/Marquee.tsx
+++ src/lib/Marquee.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import "./styles/index.css";
 
 interface Props extends React.HTMLProps<HTMLDivElement> {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 function Marquee({ children, ...rest }: Props) {
-	return <div {...rest}>{children}</div>;
+  return (
+    <div className="marquee-left" {...rest}>
+      {children}
+    </div>
+  );
 }
 
 export default Marquee;
