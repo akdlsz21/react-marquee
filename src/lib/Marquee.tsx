367f62c2e95581c8d9649cfc9bd2b37b27adc50a
Ji-hoon Choi <akdlsz21@gmail.com>
2024-08-11 18:37:23 +0900

    refactored code

--- src/lib/Marquee.tsx
+++ src/lib/Marquee.tsx
@@ -2,68 +2,53 @@ import React, { useCallback, useLayoutEffect, useRef, useState } from "react";
 import "./styles/index.css";
 
 interface Props extends React.HTMLProps<HTMLDivElement> {
-  children: React.ReactNode;
-  cb: () => void;
+	children: React.ReactNode;
+	cb: () => void;
 }
 
 function ExperimentalMarquee({ children, ...rest }: Props) {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const marqueeRef = useRef<HTMLDivElement>(null);
+	const containerRef = useRef<HTMLDivElement>(null);
+	const marqueeRef = useRef<HTMLDivElement>(null);
 
-  const [multiplier, setMultiplier] = useState(0);
-  const [containerWidth, setContainerWidth] = useState(0);
-  const [marqueeWidth, setMarqueeWidth] = useState(0);
+	const [multiplier, setMultiplier] = useState(0);
 
-  console.log(containerWidth, marqueeWidth);
+	const calculateWidth = () => {
+		if (!(marqueeRef.current && containerRef.current)) return;
+		const containerWidth = containerRef.current.getBoundingClientRect().width;
+		const childrenWidth = marqueeRef.current.getBoundingClientRect().width;
+		if (!containerWidth || !childrenWidth) {
+			setMultiplier(1);
+			return;
+		}
 
-  const calculateWidth = () => {
-    console.log(marqueeRef.current, containerRef.current, "asdfasd");
-    if (marqueeRef.current && containerRef.current) {
-      const containerRect = containerRef.current.getBoundingClientRect();
-      const marqueeRect = marqueeRef.current.getBoundingClientRect();
-      const containerWidth = containerRect.width;
-      const marqueeWidth = marqueeRect.width;
-      console.log(containerWidth, marqueeWidth);
-      if (containerWidth && marqueeWidth) {
-        setMultiplier(
-          marqueeWidth < containerWidth
-            ? Math.ceil(containerWidth / marqueeWidth) + 1
-            : 2
-        );
-      } else {
-        setMultiplier(1);
-      }
-      console.log(marqueeWidth);
-      console.log(containerWidth);
-      setContainerWidth(containerWidth);
-      setMarqueeWidth(marqueeWidth);
-    }
-  };
+		const repititions = Math.ceil(containerWidth / childrenWidth);
+		setMultiplier(repititions + 1);
+	};
 
-  useLayoutEffect(() => {
-    calculateWidth();
-  }, []);
+	useLayoutEffect(() => {
+		calculateWidth();
+	}, []);
 
-  const multiplyChildren = useCallback(
-    (multiplier: number) => {
-      return [...Array(multiplier >= 0 ? multiplier : 0)].map((_, i) => (
-        <div key={i} className="marquee-left" style={{}}>
-          {children}
-        </div>
-      ));
-    },
-    [children]
-  );
+	const multiplyChildren = useCallback(
+		(multiplier: number) => {
+			return [...Array(multiplier >= 0 ? multiplier : 0)].map((_, i) => (
+				<div key={i} className="marquee-left" style={{}}>
+					{children}
+				</div>
+			));
+		},
+		[children],
+	);
 
-  return (
-    <div ref={containerRef} {...rest}>
-      <div className="marquee-left" ref={marqueeRef} style={{}}>
-        {children}
-      </div>
+	return (
+		<div ref={containerRef} {...rest}>
+			<div className="marquee-left" ref={marqueeRef} style={{}}>
+				{children}
+			</div>
 
-      {multiplyChildren(multiplier - 1)}
-    </div>
-  );
+			{multiplyChildren(multiplier - 1)}
+		</div>
+	);
 }
 
 export default ExperimentalMarquee;
